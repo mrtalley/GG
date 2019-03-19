9a1af760bc5f249fc4c77aa23376f77ee1ccdd4c
Maxwell Talley <max.talley0@gmail.com>
2019-03-19 15:37:31 -0600

    Finished up basic design of home page on desktop

--- src/app/Home.js
+++ src/app/Home.js
@@ -22,11 +22,18 @@ class Home extends React.Component {
     }
 
     render() {
+        // Need to return the home-header-container right away
+        // only need to wait on the other images
             return this.state.images.length !== 0 ? (
                 <div className="home-container">
                     <div className="home-header-container">
                         <img src={initials} className="initials-logo" alt="Initials Logo" />
                         <img src={fullName} className="full-name-logo" alt="Full Name Logo" />
+                    </div>
+                    <div className="home-text-container">
+                        <div className="home-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+                        </div>
                     </div>
                     <PhotoGrid images={this.state.images} />
                 </div>
