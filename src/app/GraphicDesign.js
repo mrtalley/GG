3805b744f409d42972bac0e88732e30fa1297711
Maxwell Talley <max.talley0@gmail.com>
2019-03-20 12:22:36 -0600

    Added image grid to GraphicDesign

--- src/app/GraphicDesign.js
+++ src/app/GraphicDesign.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+import PhotoGrid from "./layout/PhotoGrid";
+import LoadingImages from "./layout/LoadingImages";
+import { flamelinkInstance } from "../global.js";
 
 class GraphicDesign extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { images: [] };
+
+        flamelinkInstance.content.get("graphicDesign")
+            .then(images => {
+                this.setState({ images: images });
+            })
+            .catch(error => console.log("something bad happened :-(\n", error));
+    }
+
     render() {
         return (
-            <div>Graphic Design Page</div>
+            <div>
+                Graphic Design Page
+                <PhotoGrid images={this.state.images} />
+                {this.state.images.length !== 0 ?
+                    <PhotoGrid images={this.state.images} /> : <LoadingImages />}
+            </div>
         );
     }
 }
